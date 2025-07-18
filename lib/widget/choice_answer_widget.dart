71aa90aebfd66abdda40e79cfe3c65ca2e53a224
Piyapong Ukoad <joey.exjjxe@gmail.com>
2025-07-18 12:39:51 +0700

    [update] clean code

--- lib/widget/choice_answer_widget.dart
+++ lib/widget/choice_answer_widget.dart
@@ -16,54 +16,58 @@ class _ChoiceAnswerWidgetState extends State<ChoiceAnswerWidget> {
     setState(() {
       _selectedChoiceValue = value;
     });
-    widget.onAnswer?.call(_selectedChoiceValue!);
+    widget.onAnswer?.call(_selectedChoiceValue);
   }
 
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constraints) {
-        final double horizontalPadding =
-            constraints.maxWidth * 0.05; // 5% ของความกว้าง
+        final double horizontalPadding = constraints.maxWidth * 0.05;
         return Padding(
           padding: EdgeInsets.symmetric(horizontal: horizontalPadding),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              _buildChoice(
+              _ChoiceOption(
                 image: "slight",
                 text: "Constant pain with slight fluctuations",
                 number: 1,
-                onAnswer: _handleChoiceSelection,
+                isSelected: _selectedChoiceValue == 1,
+                onSelected: _handleChoiceSelection,
               ),
               const SizedBox(height: 16),
-              _buildChoice(
+              _ChoiceOption(
                 image: "large",
                 text: "Constant pain with large fluctuations",
                 number: 2,
-                onAnswer: _handleChoiceSelection,
+                isSelected: _selectedChoiceValue == 2,
+                onSelected: _handleChoiceSelection,
               ),
               const SizedBox(height: 16),
-              _buildChoice(
+              _ChoiceOption(
                 image: "pain_peak",
                 text: "Constant pain with pain peaks",
                 number: 3,
-                onAnswer: _handleChoiceSelection,
+                isSelected: _selectedChoiceValue == 3,
+                onSelected: _handleChoiceSelection,
               ),
               const SizedBox(height: 16),
-              _buildChoice(
+              _ChoiceOption(
                 image: "pain_between",
                 text: "Pain peaks without pain between them",
                 number: 4,
-                onAnswer: _handleChoiceSelection,
+                isSelected: _selectedChoiceValue == 4,
+                onSelected: _handleChoiceSelection,
               ),
               const SizedBox(height: 16),
-              _buildChoice(
+              _ChoiceOption(
                 image: "",
                 text: "None of these pictures describe my pain",
                 number: 5,
-                onAnswer: _handleChoiceSelection,
+                isSelected: _selectedChoiceValue == 5,
+                onSelected: _handleChoiceSelection,
               ),
             ],
           ),
@@ -71,92 +75,79 @@ class _ChoiceAnswerWidgetState extends State<ChoiceAnswerWidget> {
       },
     );
   }
+}
 
-  _buildChoice({
-    required String image,
-    required String text,
-    required int number,
-    required Function(int) onAnswer,
-  }) {
-    final maxWidth = MediaQuery.of(context).size.width;
-    if (maxWidth <= 400) {
-      return Column(
-        mainAxisAlignment: MainAxisAlignment.start,
+class _ChoiceOption extends StatelessWidget {
+  const _ChoiceOption({
+    required this.image,
+    required this.text,
+    required this.number,
+    required this.isSelected,
+    required this.onSelected,
+  });
+
+  final String image;
+  final String text;
+  final int number;
+  final bool isSelected;
+  final ValueChanged<int> onSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    final double maxWidth = MediaQuery.of(context).size.width;
+
+    Widget imageWidget =
+        image.isNotEmpty
+            ? SizedBox(
+              width: 200,
+              height: 100,
+              child: Image.asset("assets/images/$image.png", fit: BoxFit.cover),
+            )
+            : const SizedBox.shrink(); // Use SizedBox.shrink() for empty space
+
+    return InkWell(
+      onTap: () => onSelected(number),
+      child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Checkbox(
-            value: _selectedChoiceValue == number,
-            onChanged: (_) => _handleChoiceSelection(number),
-          ),
-          if (image.isNotEmpty) const SizedBox(height: 5),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              if (image.isNotEmpty && image != "") ...[
-                SizedBox(
-                  width: 200,
-                  height: 100,
-                  child: Image.asset(
-                    "assets/images/$image.png",
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                const SizedBox(height: 5),
-              ],
-              Text(text),
-            ],
-          ),
-        ],
-      );
-    } else {
-      return Row(
-        children: [
-          Checkbox(
-            value: _selectedChoiceValue == number,
-            onChanged: (_) => _handleChoiceSelection(number),
-          ),
-          const SizedBox(width: 30),
-          maxWidth <= 680
-              ? Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    if (image.isNotEmpty && image != "") ...[
-                      SizedBox(
-                        width: 200,
-                        height: 100,
-                        child: Image.asset(
-                          "assets/images/$image.png",
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      const SizedBox(height: 16),
-                    ],
-                    Text(text),
-                  ],
-                ),
-              )
-              : Row(
+          Checkbox(value: isSelected, onChanged: (_) => onSelected(number)),
+          const SizedBox(width: 16), // Consistent spacing
+
+          if (maxWidth <= 400)
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  if (image.isNotEmpty && image != "") ...[
-                    SizedBox(
-                      width: 200,
-                      height: 100,
-                      child: Image.asset(
-                        "assets/images/$image.png",
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    const SizedBox(width: 16),
-                  ],
+                  imageWidget,
+                  if (image.isNotEmpty) const SizedBox(height: 8),
                   Text(text),
                 ],
               ),
+            )
+          else if (maxWidth <= 680)
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  imageWidget,
+                  if (image.isNotEmpty) const SizedBox(height: 8),
+                  Text(text),
+                ],
+              ),
+            )
+          else
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                imageWidget,
+                if (image.isNotEmpty) const SizedBox(width: 16),
+                Text(text),
+              ],
+            ),
         ],
-      );
-    }
+      ),
+    );
   }
 }
