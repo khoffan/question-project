0724f062bc508e0270b9a1e65bbcd690f048191c
khoffan <khoffan222@gmail.com>
2025-07-25 15:20:39 +0700

    update

--- lib/datasource/form_datasource.dart
+++ lib/datasource/form_datasource.dart
@@ -1,29 +1,22 @@
-import 'dart:convert';
-
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:questionnaire/model/question_model.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 abstract class FormDataSource {
   Future<List<Question>?> getQuestions();
   Future<Map<String, dynamic>?> saveFormAnswer(
     Map<String, dynamic> formAnswerModel,
   );
-
 }
 
 class FormDataSourceImpl implements FormDataSource {
-  FormDataSourceImpl({Dio? dio, SharedPreferences? sharedPreferences})
-    : _dio = dio ?? Dio(),
-      _sharedPreferences = sharedPreferences!;
+  FormDataSourceImpl({Dio? dio}) : _dio = dio ?? Dio();
 
   final Dio _dio;
   final String baseApiUrl = "http://localhost:1112";
 
-
   @override
-  Future<List<FormModel>?> getFormQuestions() async {
+  Future<List<Question>?> getQuestions() async {
     try {
       await _dio.get("$baseApiUrl/questions");
       return null;
@@ -38,15 +31,9 @@ class FormDataSourceImpl implements FormDataSource {
     Map<String, dynamic> formAnswerModel,
   ) async {
     try {
-      final key = answerKey(formAnswerModel.formId);
-      await _sharedPreferences.setString(
-        key,
-        jsonEncode(formAnswerModel.toJson()),
-      );
       final response = await _dio.post(
         "$baseApiUrl/form",
         data: formAnswerModel,
-
       );
       if (response.statusCode == 200) {
         return response.data;
